dd2762e504453f9c07444a736886b7ebb8dfd1e1
Gabriela Delamare <gabrieladnr@gmail.com>
2022-04-25 12:50:49 -0300

    Adiciona exercício numeros primos

--- fundamentos/bloco-04-introducao-a-javascript/exercicios4.3-lógica-algoritmos.js
+++ fundamentos/bloco-04-introducao-a-javascript/exercicios4.3-lógica-algoritmos.js
@@ -3,74 +3,88 @@
 // 4! = 4 x 3 x 2 x 1 = 24
 // Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
 
-let fatorial = 10
+let fatorial = 10;
 
-for (let index = 10; index > 0; index -= 1){
-    fatorial = fatorial * index ;
-
+for (let index = 10; index > 0; index -= 1) {
+  fatorial = fatorial * index;
 }
-    console.log(fatorial);
+console.log(fatorial);
 
-
-// 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab". 
+// 2- Agora, desenvolva um algoritmo que é capaz de inverter uma palavra. Por exemplo, a palavra "banana" seria invertida para "ananab".
 // Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-let word = 'açaí';
-let invertida = ""
+let word = "açaí";
+let invertida = "";
 
-for (let index = word.length-1; index >= 0; index -= 1){
-    invertida += word[index];
+for (let index = word.length - 1; index >= 0; index -= 1) {
+  invertida += word[index];
 }
 console.log(invertida);
 
 // 3- Considere o array de strings abaixo:
 // Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
 
- let array = ['java', 'javascript', 'python', 'html', 'css'];
+let array = ["java", "javascript", "python", "html", "css"];
 
- let menorPalavra = array[0];
- let maiorPalavra = array[0];
+let menorPalavra = array[0];
+let maiorPalavra = array[0];
 
- for (let index = 0; index < array.length; index += 1) {
-    if (array[index].length > maiorPalavra.length) {
-        maiorPalavra = array[index];
-    }
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index];
   }
-  
-  for (let index = 0; index < array.length; index += 1) {
-    if (array[index].length < menorPalavra.length) {
-        menorPalavra = array[index];
-    }
-  }
-  
-  console.log(maiorPalavra);
-  console.log(menorPalavra);
-  
+}
 
-// 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. 
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index];
+  }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
+
+// 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo.
 // Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
 
-// let maiorImpar = 0;
+// function maiorPrimo() {
+//   let maiorPrimo = 0;
 
-// for(let index = 0; index < 50; index += 1){
-//     if (index % 2 !== 0){
-//         maiorImpar = index;
+//   for (let index = 50; index > 2; index -= 1) {
+//     if (
+//       index % 2 !== 0 &&
+//       index % 3 !== 0 &&
+//       index % 5 !== 0 &&
+//       index % 7 !== 0
+//     ) {
+//       maiorPrimo = index;
+//       console.log(maiorPrimo);
+//       return;
 //     }
+//   }
 // }
 
-// maiorNumero = 0;
+// maiorPrimo();
 
-// for(let i = 0; i < 50; i += 1){
-//     if (i % i !== 0){
-//         maiorNumero = i;
-//     }
-// }
+function maiorPrimo(num) {
+  let maiorPrimo = 0;
 
-// maiorPrimo = 0;
+  for (let index = num; index > 2; index -= 1) {
+    let contador = 0;
+    for (let i = index - 1; i > 1; i -= 1) {
+      //o for de dentro começando pelo index -1 faz com que o num (paramentro) seja sempre dividido pelo numero seguinte a ele, e não por ele mesmo, já que ele por ele mesmo teria resto 0
+      // o for de baixo vai até 1 pra limitar a condição de teste pois todo numero é divisivel por 1
+      if (index % i === 0) {
+        contador += 1;
+        break; // break sai do for de dentro
+      }
+    }
+    if (contador === 0) { // se ele sair para essa condição, significa que o numero é primo
+      return (maiorPrimo = index);
+    }
+  }
+}
 
-// if (maiorNumero === maiorImpar){
-//     maiorPrimo = maiorNumero;
-//     console.log(maiorPrimo)
-// }
+console.log(maiorPrimo(4000));
 
-// else if ()
+
